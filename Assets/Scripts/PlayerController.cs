d1c6c8c6896e51142ca7e38d7ed142ee4447fc32
egmp7 <deweyerick@gmail.com>
2023-10-26 15:05:10 -0500

    Animations in PlayerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,13 +9,22 @@ public class PlayerController : MonoBehaviour
     [Tooltip("Move speed of the character in m/s")]
     public float MoveSpeed = 2.0f;
 
-    private Input _input;
+    // animation IDs
+    private int _animIDIsMoving;
+
     private CharacterController _controller;
+    private Animator _animator;
+    #if ENABLE_INPUT_SYSTEM 
+    private Input _input;
+    #endif
 
     void Start()
     {
         _input = GetComponent<Input>();
         _controller = GetComponent<CharacterController>();
+        _animator = GetComponent<Animator>();
+
+        AssignAnimationIDs();
     }
 
     // Update is called once per frame
@@ -23,6 +32,11 @@ public class PlayerController : MonoBehaviour
     {
         Move();
     }
+
+    private void AssignAnimationIDs()
+        {
+            _animIDIsMoving = Animator.StringToHash("isMoving");
+        }
 
     void Move()
     {
@@ -38,10 +52,14 @@ public class PlayerController : MonoBehaviour
         // Relative Positions
         Vector3 forwardRelative = playerForward * forward;
         Vector3 rightRelative = playerRight * side;
-        
+
         // Movement
         Vector3 playerMovement = forwardRelative + rightRelative;
         transform.position += playerMovement * MoveSpeed * Time.deltaTime;
-       
+
+        // animation
+        if (_input.move == Vector2.zero) _animator.SetBool(_animIDIsMoving,false);
+        else _animator.SetBool(_animIDIsMoving,true);
+
     }
 }
